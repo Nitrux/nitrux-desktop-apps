ae53e353b3b394eb71f6511018ed48b40be06214
luis-lavaire <llavaire01@gmail.com>
2020-05-22 13:21:19 -0600

    removed useless code.

--- travis-build.sh
+++ travis-build.sh
@@ -5,20 +5,18 @@ apt-get --yes install wget equivs curl git
 
 cd package/
 
-DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+deps=$(sed -r '/^(#.*|\s)*$/d; $! s/$/,/' package/dependencies | tr -d \\n)
+git_commit=$(git rev-parse --short HEAD)
 
-GIT_COMMIT=$(git rev-parse --short HEAD)
-
-echo "
-Section: misc
-Priority: optional
-Homepage: https://nxos.org
-Package: nx-desktop-apps
-Version: 0.0.2-${GIT_COMMIT}
-Maintainer: Uri Herrera <uri_herrera@nxos.org>
-Depends: $DEPENDS
-Architecture: amd64
-Description: NX Desktop Apps metapackage for Nitrux.
-" > configuration
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nx-desktop-apps" \
+	"Version: 0.0.2-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Architecture: amd64" \
+	"Description: NX Desktop Apps metapackage for Nitrux."
 
 equivs-build configuration
