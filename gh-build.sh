557051a7a17c3c321c706e4698485e41049901af
Uri Herrera <hello@uriherrera.com>
2025-11-05 08:53:16 -0600

    add dep

--- gh-build.sh
+++ gh-build.sh
@@ -35,7 +35,7 @@ git_commit=$(git rev-parse --short HEAD 2>/dev/null)
 	"Priority: required" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-desktop-apps" \
-	"Version: 1.0.1-$git_commit" \
+	"Version: 1.0.2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: all" \
